4a5d1bd85155daa8c4b32c09df335bd88cbee2b6
Jackson Roberts <jacksonroberts25@gmail.com>
2018-09-15 16:22:50 +0900

    fixed tx ordering

--- folder-server.py
+++ folder-server.py
@@ -1,5 +1,6 @@
 import falcon
 import os
+import requests
 
 demo_directory = '/Users/jacksonroberts/HackathonDemo/'
 
@@ -8,7 +9,7 @@ class FolderResource:
         folders = []
         for folder_name in os.listdir(demo_directory):
             if folder_name != '.DS_Store':
-                access_string = ""
+                access_string = "disabled"
                 if (os.access(demo_directory + folder_name, os.R_OK)):
                     access_string = "active"
                 folders.append([folder_name, access_string])
@@ -16,5 +17,18 @@ class FolderResource:
         print(folders)
         resp.set_header('Access-Control-Allow-Origin', '*')
 
+class SendTxResource:
+    def on_get(self, req, resp):
+        amount = req.get_param('amount')
+        print(amount)
+        
+        json_body = {"name":"mining","password":"","address":"H2CmE81MStvEw5pkoHDkKnkpxWeGj3sam","amount":"0.00030001","minerFee":"1"}
+        json_body['amount'] = "0." + amount
+        requests.post('http://localhost:2442/api/v1/transaction', json=json_body)
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        
+
+
 api = falcon.API()
 api.add_route('/getfolders', FolderResource())
+api.add_route('/sendtx', SendTxResource())
--- update-permissions-by-blockchain.py
+++ update-permissions-by-blockchain.py
@@ -1,8 +1,9 @@
 import requests
 import time
 import os
+from operator import itemgetter
 
-latest_processed_tx_timestamp = 1536972649888
+latest_processed_tx_timestamp = 1536991169095
 
 #print(data)
 #print(os.system("sudo chmod -R 000 /Users/jacksonroberts/HackathonDemo/Folder1"))
@@ -13,22 +14,26 @@ while True:
     print("checking for new transactions")
     r = requests.get('http://127.0.0.1:2442/api/v1/wallet/H2CmE81MStvEw5pkoHDkKnkpxWeGj3sam/txs/')
     data = r.json()
+    greatest_new_timestamp = 0
     for tx in data['txs']:
         if tx['receiveTime'] > latest_processed_tx_timestamp:
             print("new tx found, applying permissions...")
-            latest_processed_tx_timestamp = tx['receiveTime']
             amount_byte = tx['amount'][1:10]
             folder_id = int(amount_byte[1:5])
             permission_id = int(amount_byte[5: 9])
             print('perm: ' + str(permission_id))
             print('folder: ' + str(folder_id))
 
+            if (tx['receiveTime'] > greatest_new_timestamp):
+                greatest_new_timestamp = tx['receiveTime']
+
             folders = []
             for folder_name in os.listdir(demo_directory):
                 #print(folder_name)
                 if folder_name != '.DS_Store':
-                    folders.append(folder_name)
-    
+                    folders.append(folder_name.replace(" ", "\\ "))
+
+
             folders.sort()
             print(folders)
 
@@ -39,9 +44,12 @@ while True:
             elif (permission_id == 1):
                 print("sudo chmod -R 000 " + demo_directory + folders[folder_id])
                 os.system("sudo chmod -R 000 " + demo_directory + folders[folder_id])
-
+    
+    print('greatest new timestamp: ' + str(greatest_new_timestamp))
+    if (greatest_new_timestamp > latest_processed_tx_timestamp):
+        latest_processed_tx_timestamp = greatest_new_timestamp
             
-    time.sleep(5)
+    time.sleep(3)
             
             
 
